8d9c5937508c87eb50ab48ce1343a80a20d13e4e
Reza Karamad <reza.karamad@idealo.de>
2022-09-11 22:20:17 +0200

    modified local hosts file

--- etchosts.sh
+++ etchosts.sh
@@ -1,5 +1,5 @@
 echo "Setting /etc/hosts ..."
 minikube_ip=$(cat minikube_ip)
-sudo sed -i '/app.cluster.local/d' /etc/hosts
-sudo echo $minikube_ip app.cluster.local >> /etc/hosts
+sudo sed -i '/rates.cluster.local/d' /etc/hosts
+sudo echo $minikube_ip rates.cluster.local >> /etc/hosts
 echo "👍 done."
